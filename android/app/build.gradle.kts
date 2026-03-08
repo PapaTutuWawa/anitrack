5e0b109fe56c7ddc8d6f52b7c4480af7007f9594
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2026-03-08 14:30:07 +0100

    chore: Disable Android signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,9 +32,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Externally signed using a security key
+            signingConfig null
         }
     }
 }
